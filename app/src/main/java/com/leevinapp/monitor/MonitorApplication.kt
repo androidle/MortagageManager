5b8a46f4e7c0b8d82f44c5d619c45c3b16dabf92
Leevin Yang <leevin163@163.com>
2020-08-03 14:56:05 +0800

    Define andorid common gradle config file

--- app/src/main/java/com/leevinapp/monitor/MonitorApplication.kt
+++ app/src/main/java/com/leevinapp/monitor/MonitorApplication.kt
@@ -2,5 +2,4 @@ package com.leevinapp.monitor
 
 import com.leevinapp.monitor.core.common.ui.base.BaseApplication
 
-class MonitorApplication : BaseApplication() {
-}
+class MonitorApplication : BaseApplication()
